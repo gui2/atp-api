1e7ff1b779c4e7684b9b593ac56c52e8f246c08b
Nikola Peric <nikola.peric03@gmail.com>
2014-03-25 19:12:32 -0400

    travis attempt 4

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [lein-midje "3.1.3"]]}})
+                                  [lein-midje "3.1.3"]
+                                  [lein-localrepo "0.5.3"]]}})
